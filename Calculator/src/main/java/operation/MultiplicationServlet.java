50587bed7e6f3a1c9300d0a664d268b4b6223ca1
RihanaE <111240652+RihanaE@users.noreply.github.com>
2023-11-15 18:46:50 +0300

    Add files via upload

---
+++ Calculator/src/main/java/operation/MultiplicationServlet.java
@@ -0,0 +1,47 @@
+package operation;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class MultiplicationServlet
+ */
+public class MultiplicationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MultiplicationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int num1 = Integer.parseInt(request.getParameter("num1"));
+		int num2 = Integer.parseInt(request.getParameter("num2"));
+		
+		PrintWriter out = response.getWriter();
+		out.println("Answer: " + num1 * num2);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Calculator/src/main/java/operation/OperationServlet.java
@@ -0,0 +1,58 @@
+package operation;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class OperationServlet
+ */
+public class OperationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OperationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)  
+	        throws ServletException, IOException {  
+	  
+//	    response.setContentType("text/html");  
+	    PrintWriter out = response.getWriter();  
+	          
+	    String operation = request.getParameter("operation");
+	    
+	          
+	    if(operation.equals("multiply")){  
+	        RequestDispatcher rd=request.getRequestDispatcher("/MultiplicationServlet");  
+	        rd.forward(request, response);  
+	    }  
+	    else{  
+	       
+	        RequestDispatcher rd=request.getRequestDispatcher("/AdditionServlet");  
+	        rd.forward(request, response);  
+	                      
+	        }  
+	    }  
+
+}
